a04a5cbe5250237f33ccdd62fa0f7ffda5287f98
shuchingchuang1992-arch <shuchingchuang1992@gmail.com>
2026-04-10 23:28:43 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,10 @@
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
+const CACHE_NAME = 'indo-v1';
+const assets = ['/', '/index.html', '/style.css', '/script.js'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
-self.addEventListener('activate', (event) => {
-  console.log('SW active');
-});
-
-self.addEventListener('fetch', (event) => {
-  // 保持空白即可
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
